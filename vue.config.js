35b65cb6f428b9e73408f354f597724cccd41b50
Tomasz Tomczyk <me@tomasztomczyk.com>
2020-07-03 15:46:37 +0100

    Try another combination of configuration

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        productName: 'Arcfire',
+        appId: 'com.arcfire.app'
+      },
+    },
+  }
+}
